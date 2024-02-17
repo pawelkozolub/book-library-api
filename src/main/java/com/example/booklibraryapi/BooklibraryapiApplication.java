4b6e6b2073eeddc68e0a22688cf900aaa4007563
pawelkozolub <pkozolub@gmail.com>
2024-02-17 23:23:01 +0100

    Initial commit

---
+++ src/main/java/com/example/booklibraryapi/BooklibraryapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.booklibraryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooklibraryapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooklibraryapiApplication.class, args);
+	}
+
+}
