79075cf6c82bbcf9c4443b632d9b7f2728dc6ac9
pawelkozolub <pkozolub@gmail.com>
2024-02-18 20:30:30 +0100

    Add BookController with getAll method.

---
+++ src/main/java/com/example/booklibraryapi/BookController.java
@@ -0,0 +1,24 @@
+package com.example.booklibraryapi;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class BookController {
+
+    @Autowired
+    BookRepository bookRepository;
+
+    @GetMapping("/test")
+    public int test() {
+        return 0;
+    }
+
+    @GetMapping("/books")
+    public List<Book> getAll() {
+        return bookRepository.getAll();
+    }
+}
